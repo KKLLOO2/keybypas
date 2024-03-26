30cd7206e8ff92e27ce6b33951ae5f04d213efc5
KKLLOO2 <164794787+KKLLOO2@users.noreply.github.com>
2024-03-27 00:32:40 +0100

    Add files via upload
    s

--- serial_checker.bat
+++ serial_checker.bat
@@ -1,8 +1,6 @@
 ECHO OFF
-TITLE HWID Checker
-ECHO **********************************
+TITLE serialchecker
 Color 0F
-ECHO **********************************
 :start
 cls
 wmic diskdrive get model, serialnumber
@@ -15,6 +13,4 @@ wmic baseboard get serialnumber
 ECHO smBIOS UUID
 wmic path win32_computersystemproduct get uuid
 getmac
-echo Press any key to get your hardware serials again.
 pause>nul
-goto start
